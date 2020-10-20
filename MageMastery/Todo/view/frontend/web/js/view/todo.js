86710b3f65210d394dd263f59b89b714207546dd
Carl Simpson <carl.simpson@maginus.com>
2020-10-20 06:34:37 +0100

    Lesson 27: Creating logic to add a new task into the pool of tasks

--- MageMastery/Todo/view/frontend/web/js/view/todo.js
+++ MageMastery/Todo/view/frontend/web/js/view/todo.js
@@ -1,63 +1,74 @@
 define([
-    'uiComponent',
-    'jquery',
-    'Magento_Ui/js/modal/confirm'
-], function (Component, $ , modal) {
-    'use strict';
+  'uiComponent',
+  'jquery',
+  'Magento_Ui/js/modal/confirm'
+], function (Component, $, modal) {
+  'use strict';
 
-    return Component.extend({
-        defaults: {
-            tasks: [
-                {id: 1, label: "Task 1", status: false},
-                {id: 2, label: "Task 2", status: false},
-                {id: 3, label: "Task 3", status: false},
-                {id: 4, label: "Task 4", status: true},
-            ],
-        },
+  return Component.extend({
+    defaults: {
+      newTaskLabel: ' ',
+      buttonSelector: ' ',
+      tasks: [
+        {id: 1, label: "Task 1", status: false},
+        {id: 2, label: "Task 2", status: false},
+        {id: 3, label: "Task 3", status: false},
+        {id: 4, label: "Task 4", status: true},
+      ],
+    },
 
-        initObservable: function () {
-            this._super().observe(['tasks']);
+    initObservable: function () {
+      this._super().observe(['tasks', 'newTaskLabel']);
 
-            return this;
-        },
+      return this;
+    },
 
-        switchStatus: function (data, event) {
-            const taskId = $(event.target).data('id');
+    switchStatus: function (data, event) {
+      const taskId = $(event.target).data('id');
 
-            var items = this.tasks().map(function (task) {
-                if (task.id === taskId) {
-                    task.status = !task.status;
-                }
-                return task
+      var items = this.tasks().map(function (task) {
+        if (task.id === taskId) {
+          task.status = !task.status;
+        }
+        return task
+      });
+
+      this.tasks(items);
+    },
+
+    deleteTask: function (taskId) {
+      const self = this;
+
+      modal({
+        content: 'Are you sure you want to delete the task?',
+        actions: {
+          confirm: function() {
+            var tasks = [];
+
+            if (self.tasks().length === 1) {
+              self.tasks(tasks);
+              return;
+            }
+
+            self.tasks().forEach(function (task) {
+              if (task.id !== taskId) {
+                tasks.push(task);
+              }
             });
 
-            this.tasks(items);
+            self.tasks(tasks);
+          }
         },
+      });
+    },
 
-        deleteTask: function (taskId) {
-            const self = this;
-
-            modal({
-                content: 'Are you sure you want to delete the task?',
-                actions: {
-                    confirm: function() {
-                        var tasks = [];
-
-                        if (self.tasks().length === 1) {
-                            self.tasks(tasks);
-                            return;
-                        }
-
-                        self.tasks().forEach(function (task) {
-                            if (task.task_id !== taskId) {
-                                tasks.push(task);
-                            }
-                        });
-
-                        self.tasks(tasks);
-                    }
-                },
-            });
-        },
-    });
+    addTask: function () {
+      this.tasks.push({
+        id: Math.floor(Math.random() * 100),
+        label: this.newTaskLabel(),
+        status: false
+      });
+      this.newTaskLabel('');
+    },
+  });
 });
